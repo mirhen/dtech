d13d39b5cce56701ac4669f32314e508bacaba11
Miriam Haart <miriamhaart@Miriams-MacBook-Pro-2.local>
2017-10-01 12:53:15 -0700

    first commit

---
+++ Pods/Target Support Files/Pods-DTech High/Pods-DTech High-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DTech_High : NSObject
+@end
+@implementation PodsDummy_Pods_DTech_High
+@end
---
+++ Pods/Target Support Files/Pods-DTech HighUITests/Pods-DTech HighUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DTech_HighUITests : NSObject
+@end
+@implementation PodsDummy_Pods_DTech_HighUITests
+@end
