02402be6d7a857731bbf68277671b5cd41e06537
Miriam Haart <miriamhaart@miriams-macbook-pro-2.local>
2017-09-30 23:22:46 -0700

    Initial Commit

---
+++ DTech High/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DTech High
+//
+//  Created by Miriam Haart on 9/30/17.
+//  Copyright © 2017 Miriam Haart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
